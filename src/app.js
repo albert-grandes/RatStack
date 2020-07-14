6086ee1ccf7cc9b9e283912212d062ef4d475a2d
albert-grandes <agrandes11@outlook.es>
2020-07-14 13:19:55 +0200

    Show actual folder

--- src/app.js
+++ src/app.js
@@ -7,26 +7,30 @@ Index:
 /************************************************************************************/
 // S> 2. Automatic run
 $(document).ready(function(){
-    //We have to import the data of tree folder!
+    //We have to import the data of tree folder! 
+    loadTreeFolder()
+    //We execute the tree folder function
+    //treeFolder()
+})
+// E> 2. Automatic run
+/************************************************************************************/
+
+/************************************************************************************/
+// S> 3. Info page loader
+function loadTreeFolder() {
     $.ajax("php/rootStruct.php")
     .done(function(data) {
         addFolders("folderTree", data); //Recursive function
         function addFolders(id, fobject) {
             for (key in fobject) {
                 const pathDir = fobject[key]["path"];
-                console.log(fobject[key]["path"]);
                 const folder = $("<li>")
                 .append(
                     $("<span>", {class: "folder", text: key})
                     .attr("data-path", fobject[key]["path"])
                     .click(function(){
                         //In this place we can make and ajax that change the content of folderScreen__files and folder                        
-                        $.post("php/dirStruct.php", {
-                            path: pathDir
-                        })
-                        .done(function(data) {
-                            console.log(data)
-                        })
+                        showFolder(pathDir);
                     }))
                 .append(
                     $("<ul>", {class: "nested", id: `id${key}`})
@@ -39,14 +43,107 @@ $(document).ready(function(){
     })
     .fail(function() {
         alert( "error" );
-    })
-    .always(function() {
-        /*alert( "complete" );*/
     });
-    //We execute the tree folder function
-    //treeFolder()
-})
-// E> 2. Automatic run
+}
+
+function showFolder(pathDir) {
+    /*
+    <div id="folderStruct">
+                <div id="fs-header">
+                    <span></span>
+                    <span>Name</span>
+                    <span>Size</span>
+                    <span>Creation</span>
+                    <span>Last Mod.</span>
+                </div>
+                <div class="fs-card">
+                    <span><img src="folder.png"></span>
+                    <span>Potato<span class="card-name-extension"></span></span>
+                    <span>45B</span>
+                    <span>24/12/2000</span>
+                    <span>05/01/2001</span>
+                </div>
+            </div>
+    */
+    $.post("php/dirStruct.php", {
+        path: pathDir
+    })
+    .done(function(data) {
+        console.log(data);
+        //First we load the header of the table! :)
+        $("#folderStruct").empty()
+        .append(
+            $("<div>", {id:"fs-header"})
+            .append(
+                $("<span>")
+            )
+            .append(
+                $("<span>", {text:"Name"})
+            )
+            .append(
+                $("<span>", {text:"Size"})
+            )
+            .append(
+                $("<span>", {text:"Creation"})
+            )
+            .append(
+                $("<span>", {text:"Last Mod."})
+            )
+        )
+        for (file in data) {
+            if(data[file]["type"]=="dir") {
+                var classFile = "fs-card-dir";
+            } else {
+                var classFile = "fs-card-file";
+            }
+            $("#folderStruct")
+            .append(
+            $("<div>", {id:"fs-card", class: classFile})
+            .append(
+                $("<span>")
+                .append(
+                    /*<img src="folder.png"></img>*/
+                    $("<img>",{src:"folder.png"})
+                )
+            )
+            .append(
+                $("<span>", {text:file})
+            )
+            .append(
+                $("<span>", {text:"Size"})
+            )
+            .append(
+                $("<span>", {text:"Creation"})
+            )
+            .append(
+                $("<span>", {text:"Last Mod."})
+            )
+        )
+        }
+        /*
+        addFile("folderStruct", data); //Recursive function
+        function addFile(id, fobject) {
+            for (key in fobject) {
+                const pathDir = fobject[key]["path"];
+                const folder = $("<li>")
+                .append(
+                    $("<span>", {class: "folder", text: key})
+                    .attr("data-path", fobject[key]["path"])
+                    .click(function(){
+                        //In this place we can make and ajax that change the content of folderScreen__files and folder                        
+                        showFolder(pathDir);
+                    }))
+                .append(
+                    $("<ul>", {class: "nested", id: `id${key}`})
+                )
+                $(`#${id}`).append(folder)
+                addFolders(`id${key}`, fobject[key]["content"])
+            }
+        }*/
+    })
+}
+
+// E> 3. Info page loader
 /************************************************************************************/
 
 /************************************************************************************/
@@ -54,8 +151,6 @@ $(document).ready(function(){
 function treeFolder() {
     console.log("treeFolder function is active! ")
     $(".folder").click(function(){
-        console.log("We are working!");
-        console.log($(this).next().html());
         $(this).next(".nested").toggleClass("active");
         $(this).toggleClass("folder-down");
     })
